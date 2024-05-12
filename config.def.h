2b4e96aebcd03063e1330bd2a4f0807e2e9cfca4
tw010 <twoten.eq@gmail.com>
2024-05-12 13:34:33 +0300

    ignore

--- config.def.h
+++ config.def.h
@@ -39,6 +39,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
+	{NULL,	NULL,	NULL,	0,	1,	-1}
 };
 
 /* layout(s) */
